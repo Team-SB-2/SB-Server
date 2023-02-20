e1c1ebdb0df416c89fecc98d563efe9325bb6f15
seongwon <seongwon1290@dsm.hs.kr>
2023-02-20 15:17:20 +0900

    ⚙️ :: 패키지 구조 설정

---
+++ SB-Server/src/main/java/com/example/sbserver/SbServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbServerApplication.class, args);
+	}
+
+}
