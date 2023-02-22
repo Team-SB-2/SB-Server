29183654fe3b2f312f8fc0a87a850f4aee799834
seongwon <seongwon1290@dsm.hs.kr>
2023-02-22 14:48:43 +0900

    ♻️ :: 필터 추가

--- SB-Server/src/main/java/com/example/sbserver/global/security/SecurityConfig.java
+++ SB-Server/src/main/java/com/example/sbserver/global/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.sbserver.global.security;
 
+import com.example.sbserver.global.config.FilterConfig;
+import com.example.sbserver.global.security.jwt.JwtParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +16,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final ObjectMapper objectMapper;
+    private final JwtParser jwtParser;
 
 
     @Bean
@@ -21,8 +26,14 @@ public class SecurityConfig {
                 .csrf().disable()
                 .formLogin().disable()
                 .cors()
+
                 .and()
-                .build();
+                .authorizeRequests()
+                .anyRequest().permitAll()
+
+                .and()
+                .apply(new FilterConfig(jwtParser, objectMapper))
+                .and().build();
     }
 
     @Bean
