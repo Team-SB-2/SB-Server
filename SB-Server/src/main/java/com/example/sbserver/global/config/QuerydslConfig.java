1829ca140ab949da0ff16533b5f62d4892838ed1
seongwon <seongwon1290@dsm.hs.kr>
2023-03-12 14:19:13 +0900

    ♻️ :: JPAQueryFactory 추가

--- SB-Server/src/main/java/com/example/sbserver/global/config/QuerydslConfig.java
+++ SB-Server/src/main/java/com/example/sbserver/global/config/QuerydslConfig.java
@@ -1,5 +1,6 @@
 package com.example.sbserver.global.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
