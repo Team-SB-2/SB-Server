f4904c7692008de8efd5b3eb67fe3c407987eb6e
seongwon <seongwon1290@dsm.hs.kr>
2023-03-12 14:22:58 +0900

    📝 :: CreateSubjectRequest

---
+++ SB-Server/src/main/java/com/example/sbserver/domain/subject/presentation/dto/request/CreateSubjectRequest.java
@@ -0,0 +1,19 @@
+package com.example.sbserver.domain.subject.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@Getter
+public class CreateSubjectRequest {
+
+    @NotBlank(message = "title은 앞글자 띄어쓰기, Null을 허용하지 않습니다")
+    @Size(min = 1, max = 20, message = "title은 1~20글자 이내여야 합니다")
+    private String title;
+
+    @NotBlank(message = "emoji는 앞글자 띄어쓰기, Null을 허용하지 않습니다")
+    private String emoji;
+}
