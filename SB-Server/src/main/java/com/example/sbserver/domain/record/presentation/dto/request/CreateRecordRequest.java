64b634926139558b45d1ae03e21964496ceb1d73
seongwon <seongwon1290@dsm.hs.kr>
2023-03-12 14:24:01 +0900

    📝 :: CreateRecordRequest

---
+++ SB-Server/src/main/java/com/example/sbserver/domain/record/presentation/dto/request/CreateRecordRequest.java
@@ -0,0 +1,21 @@
+package com.example.sbserver.domain.record.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class CreateRecordRequest {
+
+    @NotBlank(message = "started_time은 앞글자 띄어쓰기, Null을 허용하지 않습니다")
+    private LocalDateTime startedTime;
+
+    @NotBlank(message = "finished_time은 앞글자 띄어쓰기, Null을 허용하지 않습니다")
+    private LocalDateTime finishedTime;
+
+    @NotBlank(message = "memo는 앞글자 띄어쓰기, Null을 허용하지 않습니다")
+    private String memo;
+}
