50d1b31c8c8c9fb202e7586e5a81cd6fb11776a7
seongwon <seongwon1290@dsm.hs.kr>
2023-03-13 20:54:18 +0900

    📝 :: QueryMySubjectsService

---
+++ SB-Server/src/main/java/com/example/sbserver/domain/subject/service/QueryMySubjectsService.java
@@ -0,0 +1,36 @@
+package com.example.sbserver.domain.subject.service;
+
+import com.example.sbserver.domain.subject.domain.repository.SubjectRepository;
+import com.example.sbserver.domain.subject.domain.repository.vo.SubjectVo;
+import com.example.sbserver.domain.subject.presentation.dto.response.QuerySubjectResponse;
+import com.example.sbserver.domain.user.domain.User;
+import com.example.sbserver.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class QueryMySubjectsService {
+    private final SubjectRepository subjectRepository;
+    private final UserFacade userFacade;
+
+    @Transactional(readOnly = true)
+    public List<QuerySubjectResponse> execute() {
+        User user = userFacade.getCurrentUser();
+        List<SubjectVo> subjects = subjectRepository.findAllByUser(user);
+
+        return subjects.stream().map(
+                        subject -> QuerySubjectResponse.builder()
+                                .postId(subject.getPostId())
+                                .title(subject.getTitle())
+                                .emoji(subject.getEmoji())
+                                .userId(subject.getUserId())
+                                .build()
+                )
+                .collect(Collectors.toList());
+    }
+}
