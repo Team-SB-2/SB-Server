f300a3eda840c6456d57198e73d5a671f70f9ac1
seongwon <seongwon1290@dsm.hs.kr>
2023-03-19 16:26:47 +0900

    ♻️ :: ConfigurationPropertiesScan

--- SB-Server/src/main/java/com/example/sbserver/SbServerApplication.java
+++ SB-Server/src/main/java/com/example/sbserver/SbServerApplication.java
@@ -2,8 +2,11 @@ package com.example.sbserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SbServerApplication {
 
 	public static void main(String[] args) {
