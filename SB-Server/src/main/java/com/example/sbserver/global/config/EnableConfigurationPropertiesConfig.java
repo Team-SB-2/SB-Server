eea21361f2f69c69604064e86f85af35e4e92827
seongwon <seongwon1290@dsm.hs.kr>
2023-03-19 16:27:07 +0900

    ♻️ :: redisConfig도 Enable로 변경

--- SB-Server/src/main/java/com/example/sbserver/global/config/EnableConfigurationPropertiesConfig.java
+++ SB-Server/src/main/java/com/example/sbserver/global/config/EnableConfigurationPropertiesConfig.java
@@ -3,6 +3,6 @@ package com.example.sbserver.global.config;
 import com.example.sbserver.global.security.jwt.JwtProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(value = {JwtProperties.class})
+@EnableConfigurationProperties(value = {JwtProperties.class, RedisConfig.class})
 public class EnableConfigurationPropertiesConfig {
 }
