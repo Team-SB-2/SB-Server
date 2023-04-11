4e16b000738078e2c70d1e45c52c5bc613badd9e
seongwon <seongwon1290@dsm.hs.kr>
2023-04-11 11:29:31 +0900

    ♻️ :: todayRecord 추가

--- SB-Server/src/main/java/com/example/sbserver/domain/subject/domain/repository/vo/SubjectVo.java
+++ SB-Server/src/main/java/com/example/sbserver/domain/subject/domain/repository/vo/SubjectVo.java
@@ -1,22 +1,22 @@
 package com.example.sbserver.domain.subject.domain.repository.vo;
 
-import com.example.sbserver.domain.subject.domain.Subject;
-import com.example.sbserver.domain.user.domain.User;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
 @Getter
 public class SubjectVo {
-    private final Long postId;
+    private final Long subjectId;
     private final String title;
     private final String emoji;
     private final Long userId;
+    private final int todayRecord;
 
     @QueryProjection
-    public SubjectVo(Subject subject, User user) {
-        this.postId = subject.getId();
-        this.title = subject.getTitle();
-        this.emoji = subject.getEmoji();
-        this.userId = user.getId();
+    public SubjectVo(Long subjectId, String title, String emoji, Long userId, int todayRecord) {
+        this.subjectId = subjectId;
+        this.title = title;
+        this.emoji = emoji;
+        this.userId = userId;
+        this.todayRecord = todayRecord;
     }
 }
