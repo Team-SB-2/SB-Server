49f7931c34da69eeba67d5796bf47fa6d8bd5c50
seongwon <seongwon1290@dsm.hs.kr>
2023-05-13 11:56:45 +0900

    ♻️ :: 글자수 제약 변경

--- src/main/java/com/example/sbserver/domain/tips/domain/Tips.java
+++ src/main/java/com/example/sbserver/domain/tips/domain/Tips.java
@@ -17,7 +17,7 @@ public class Tips extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 20, nullable = false)
+    @Column(length = 100, nullable = false)
     private String title;
 
     @Column(length = 1000, nullable = false)
