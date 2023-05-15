494a9678b672c9aa11eb31ef0685896948da3d65
seongwon <seongwon1290@dsm.hs.kr>
2023-05-15 12:21:07 +0900

    ♻️ :: totalTime 필드 추가

--- src/main/java/com/example/sbserver/domain/record/presentation/dto/response/QueryRecordedDaysResponse.java
+++ src/main/java/com/example/sbserver/domain/record/presentation/dto/response/QueryRecordedDaysResponse.java
@@ -3,10 +3,12 @@ package com.example.sbserver.domain.record.presentation.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.YearMonth;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class QueryRecordedDaysResponse {
+    private YearMonth yearMonth;
     private List<Integer> recordedDays;
 }
