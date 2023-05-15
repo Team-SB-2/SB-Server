34d18fbd1bb4964b155b3a092c733516d47b7f7a
seongwon <seongwon1290@dsm.hs.kr>
2023-05-16 08:41:44 +0900

    ♻️ :: focusedRatio 추가

--- src/main/java/com/example/sbserver/domain/analysis/presentation/dto/response/FocusResponse.java
+++ src/main/java/com/example/sbserver/domain/analysis/presentation/dto/response/FocusResponse.java
@@ -3,13 +3,16 @@ package com.example.sbserver.domain.analysis.presentation.dto.response;
 import com.example.sbserver.domain.record.domain.repository.vo.FocusVo;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Builder
+@Setter
 public class FocusResponse {
     private final String title;
     private final String emoji;
     private final int sum;
+    private int focusedRatio;
 
     public static FocusResponse of(FocusVo focusVo) {
         return FocusResponse.builder()
@@ -17,5 +20,10 @@ public class FocusResponse {
                 .emoji(focusVo.getEmoji())
                 .sum(focusVo.getSum())
                 .build();
+    }
+
+    public FocusResponse updateFocusRatio(int focusedRatio) {
+        this.focusedRatio = focusedRatio;
+        return this;
     }
 }
