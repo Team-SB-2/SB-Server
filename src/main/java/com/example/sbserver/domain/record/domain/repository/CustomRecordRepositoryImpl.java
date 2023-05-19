a5b3c1483893eae3557a766b8812500fb692e5be
seongwon <seongwon1290@dsm.hs.kr>
2023-05-19 11:50:06 +0900

    ♻️ :: isRecord=true인 것만 select

--- src/main/java/com/example/sbserver/domain/record/domain/repository/CustomRecordRepositoryImpl.java
+++ src/main/java/com/example/sbserver/domain/record/domain/repository/CustomRecordRepositoryImpl.java
@@ -41,7 +41,8 @@ public class CustomRecordRepositoryImpl implements CustomRecordRepository {
                 .innerJoin(record.subject, subject)
                 .groupBy(subject.id)
                 .where(record.user.eq(user), record.finishedTime.between(yearMonth.atDay(1).atStartOfDay(),
-                        yearMonth.atEndOfMonth().atTime(LocalTime.MAX)))
+                        yearMonth.atEndOfMonth().atTime(LocalTime.MAX))
+                        .and(record.isRecord.isTrue()))
                 .fetch();
     }
 
@@ -50,7 +51,8 @@ public class CustomRecordRepositoryImpl implements CustomRecordRepository {
         return jpaQueryFactory.select(record.total.sum().coalesce(0))
                 .from(record)
                 .where(record.user.eq(user)
-                        .and(record.finishedTime.between(localDateTime, localDateTime.minusDays(30))))
+                        .and(record.finishedTime.between(localDateTime, localDateTime.minusDays(30))
+                                .and(record.isRecord.isTrue())))
                 .fetchOne();
     }
 
@@ -68,7 +70,9 @@ public class CustomRecordRepositoryImpl implements CustomRecordRepository {
         return jpaQueryFactory.selectDistinct(record.finishedTime.dayOfMonth())
                 .from(record)
                 .where(record.finishedTime.between(yearMonth.atDay(1).atStartOfDay(),
-                        yearMonth.atEndOfMonth().atTime(LocalTime.MAX)).and(record.user.eq(user)))
+                        yearMonth.atEndOfMonth().atTime(LocalTime.MAX))
+                        .and(record.user.eq(user)
+                        .and(record.isRecord).isTrue()))
                 .orderBy(record.finishedTime.dayOfMonth().asc())
                 .fetch();
     }
